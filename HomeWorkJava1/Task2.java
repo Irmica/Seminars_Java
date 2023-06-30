e2be5b3f58d285c71c5b06aa7e8e5cdaec3a92ca
Irmica <idali@list.ru>
2023-06-30 18:22:55 +0300

    Corrections HomeWork1 Task2

--- HomeWorkJava1/Task2.java
+++ HomeWorkJava1/Task2.java
@@ -1,27 +1,27 @@
+//2) Вывести все простые числа от 1 до 1000
 package HomeWorkJava1;
 
+import java.util.Scanner;
+
 public class Task2 {
-    public class PrimeNumbers {
+
     public static void main(String[] args) {
-        int i, j;
-        boolean isPrime;
+        System.out.print("Введите n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.printf("Простые числа от 1 до %d:\n2\n", n);
+        primeNumbers(n);
+    }
 
-        System.out.println("Простые числа от 1 до 1000:");
-
-        for (i = 2; i <= 1000; i++) {
-            isPrime = true;
-
-            for (j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
+    static void primeNumbers(int n) {
+        for (int i = 3; i <= n; i += 2) {
+            boolean c = true;
+            for(int j = 2; j < i; j++){
+                if(i%j==0) c =false;
                 }
-            }
-
-            if (isPrime) {
-                System.out.println(i);
+            if(c) System.out.println(i);
             }
         }
-    }
-}
+
 }
