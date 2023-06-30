e63f42e2663a105483eda2ac632185b696d846de
Irmica <idali@list.ru>
2023-06-30 17:30:51 +0300

    Сorrections HomeWork1 Task1

--- HomeWorkJava1/Task1.java
+++ HomeWorkJava1/Task1.java
@@ -1,36 +1,34 @@
-//1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+//Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
 package HomeWorkJava1;
 
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
         System.out.print("Введите n: ");
         int n = scanner.nextInt();
         scanner.close();
-        int sum = triangular_number(n);
+        int sum = amount(n);
         int factorial = factorial(n);
         System.out.println("Сумма чисел от 1 до " + n + " равна " + sum);
         System.out.println(n + "! = " + factorial);
-
+}
+    static int amount(int x){
+        int s = 0;
+        for (int i =1; i<=10; i++){
+            s+=i;
+        }
+        return s;
     }
 
-    static int triangular_number(int number) {
-        int sum = 0;
-        for (int i = 1; i <= number; i++) {
-            sum += i;
+    static int factorial(int x){
+        int f = 0;
+        for (int i =1; i<=10; i++){
+            f*=i;
         }
-        return sum;
-    }
-
-    static int factorial(int number) {
-        int factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
-        }
-        return factorial;
+        return f;
     }
 
 }
