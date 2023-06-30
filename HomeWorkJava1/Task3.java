28dca5412d3e43e0862da1146c5df3bbee23dbe4
Irmica <idali@list.ru>
2023-06-30 19:15:12 +0300

    Correction HomeWork1 Task3

--- HomeWorkJava1/Task3.java
+++ HomeWorkJava1/Task3.java
@@ -1,43 +1,56 @@
-//3) Реализовать простой калькулятор
-
 package HomeWorkJava1;
 
 import java.util.Scanner;
 
+/**
+ * Task3 Реализовать простой калькулятор
+ */
 public class Task3 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Введите оператор (+, -, *, /): ");
-        char operator = input.next().charAt(0);
-
-        System.out.print("Введите первое число: ");
-        double num1 = input.nextDouble();
-        System.out.print("Введите второе число: ");
-        double num2 = input.nextDouble();
-
-        double result;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(
+                "Введите выражение, которое необходимо рассчитать, разделяя числа и знаки действия пробелами: ");
+        int a = sc.nextInt();
+        String d = sc.next();
+        int b = sc.nextInt();
+        sc.close();
+        switch (d) {
+            case "+":
+                int result1 = addition(a, b);
+                System.out.printf("%d %s %d = %d", a, d, b, result1);
                 break;
-            case '-':
-                result = num1 - num2;
+            case "-":
+                int result2 = subtraction(a, b);
+                System.out.printf("%d %s %d = %d", a, d, b, result2);
                 break;
-            case '*':
-                result = num1 * num2;
+            case "*":
+                int result3 = multiplication(a, b);
+                System.out.printf("%d %s %d = %d", a, d, b, result3);
                 break;
-            case '/':
-                result = num1 / num2;
+            case "/":
+                double result4 = division(a, b);
+                System.out.printf("%d %s %d = %.2f", a, d, b, result4);
                 break;
             default:
                 System.out.println("Ошибка! Неправильный оператор.");
-                return;
         }
+    }
 
-    
-        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    static int addition(int a, int b) {
+        return a + b;
+    }
+
+    static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    static double division(int a, int b) {
+        double a1 = a;
+        return (a1 / b);
     }
 }
