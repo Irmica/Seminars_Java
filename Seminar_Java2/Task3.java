359f93dcd883f68e4d3a82324d576f42b9cb225a
Irmica <idali@list.ru>
2023-07-01 17:34:21 +0300

    Correction Seminar2 Task3

--- Seminar_Java2/Task3.java
+++ Seminar_Java2/Task3.java
@@ -4,6 +4,15 @@ public class Task3 {
     public static void main(String[] args) {
         // Напишите метод, который принимает на вход строку (String)
         // и определяет является ли строка палиндромом (возвращает boolean значение).
-
+System.out.println(polindrom("aaanmnaaa"));
     }
+
+    static boolean polindrom(String str) {
+        StringBuilder res = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--)
+            res.append(str.charAt(i));
+
+        return str.equals(res.toString());
+    }
+
 }
