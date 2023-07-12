af0933c9fd0da7bb0ba6a607e8a175eeac32695a
Irmica <idali@list.ru>
2023-07-12 21:16:29 +0300

    Add HomeWork6

---
+++ HomeWorkJava6/Task1.java
@@ -0,0 +1,14 @@
+package HomeWorkJava6;
+
+// Реализуйте класс Cat, включающий в себя от трех полей и от двух методов, определяющих базовый функционал класса(за основу можно взять наработки с семинара). Создать два экземпляра класса Cat и вывести информацию о них на экран.
+public class Task1 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Барсик", 5, 'm', 6.5);
+        Cat cat2 = new Cat("Мурка", 3, 'f', 4.2);
+
+        cat1.drink();
+        System.out.println(cat2.speak());
+        cat1.aboutCat();
+        cat2.aboutCat();
+    }
+}
